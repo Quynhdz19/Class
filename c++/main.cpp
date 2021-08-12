1f207adcd1bb96c822bd57784c2e1d2877fa1bdb
fhdiwi <lexuanquynh0123@gmai.com>
2021-08-13 00:47:56 +0700

    add cat

--- c++/main.cpp
+++ c++/main.cpp
@@ -5,10 +5,87 @@
 //  Created by mac on 12/08/2021.
 //
 
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Node{
+public:
+    int value;
+    Node * next;
+};
+Node * creatNode(int x){
+    Node * temp = new Node;
+    temp -> next = NULL;
+    temp -> value = x;
+    return temp;
+}
+Node * addListNode(Node * p,int x){
+    Node * temp = creatNode(x);
+    p -> next = temp;
+    return temp;
+    
+}
+void print(Node * l,int x){
+    Node * p = l;
+    while(p != NULL){
+        cout << p -> value << " ";
+        p = p -> next;
+    }
+}
+Node * addHead(Node * l,int x){
+    
+    Node * temp = new Node;
+    temp -> value = x;
+    temp -> next = l;
+    l = temp;
+    return l;
+    
+}
+Node * Tail(Node * l,int x){
+    Node * p = l;
+    while (p -> next != NULL) {
+        p = p -> next;
+    }
+    Node * temp = new Node;
+    temp -> value = x;
+    temp -> next = NULL;
+    p -> next = temp;
+    return l;
+    
+}
+Node * At(Node * l,int x,int k){
+    Node * p = l;
+    for (int i = 0; i < k -1; i++) {
+        p = p -> next;
+    }
+    Node * temp = new Node;
+    temp -> value = x;
+    temp -> next = p -> next;
+    p -> next = temp;
+    return l;
+}
+int main() {
+    int n,x,k;
+    cin >> n;
+    cin >> x;
+    Node * l = creatNode(x);
+    Node * p = l;
+    for(int i = 1;i < n;i++){
+        cin >> x;
+        p = addListNode(p, x);
+    }
+    cin >> x >> k;
+    if ( k == 0){
+        l = addHead(l, x);
+    }
+    else if (k == n) {
+        l = Tail(l, x);
+    }
+    else {
+        l = At(l, x, k);
+    }
+    print(l, x);
     return 0;
 }
+                        
+
